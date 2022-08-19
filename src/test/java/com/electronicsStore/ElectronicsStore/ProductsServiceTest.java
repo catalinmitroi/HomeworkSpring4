bb7f1c3e36a997770e625e0a26b3f51c80ecb793
catalinmitroi <catalinmitroi18@yahoo.com>
2022-08-19 21:39:37 +0300

    Added Unit Test File Updated.

--- src/test/java/com/electronicsStore/ElectronicsStore/ProductsServiceTest.java
+++ src/test/java/com/electronicsStore/ElectronicsStore/ProductsServiceTest.java
@@ -30,4 +30,6 @@ public class ProductsServiceTest {
         assertThat(p.getName()).isSameAs("Frigider");
 
     }
+
+
 }
--- src/test/java/com/electronicsStore/ElectronicsStore/ShoppingCartServiceTest.java
+++ src/test/java/com/electronicsStore/ElectronicsStore/ShoppingCartServiceTest.java
@@ -1,4 +1,32 @@
 package com.electronicsStore.ElectronicsStore;
 
+import com.electronicsStore.ElectronicsStore.model.Products;
+import com.electronicsStore.ElectronicsStore.model.ShoppingCart;
+import com.electronicsStore.ElectronicsStore.service.ProductsService;
+import com.electronicsStore.ElectronicsStore.service.ShoppingCartService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.electronicsStore.ElectronicsStore.model.Type.ELB;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ShoppingCartService.class)
 public class ShoppingCartServiceTest {
+
+    @MockBean
+    ShoppingCartService shoppingCartService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void it_should_create_shoppingCart() throws Exception{
+        shoppingCartService.createShoppingCart();
+
+    }
 }
